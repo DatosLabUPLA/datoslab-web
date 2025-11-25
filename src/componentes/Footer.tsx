1afe85de5ec624252617e265abbbc2cb8d1da00d
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2025-11-24 21:43:37 -0300

    Update footer logos and social link
    Replaced DatosLab logo and UPLA logo images in the footer, updated their sizes, and changed the Twitter link to X. Added new DatosLab logo image to the project.

--- src/componentes/Footer.tsx
+++ src/componentes/Footer.tsx
@@ -12,9 +12,9 @@ const Footer: React.FC = () => {
           {/* Logo UPLA a la izquierda */}
           <a href="https://www.upla.cl" target="_blank" rel="noopener noreferrer" className="flex-shrink-0">
             <img
-              src="/images/img/logoupla_bn.png"
+              src="/images/img/logo_upla.png"
               alt="Universidad de Playa Ancha"
-              className="max-h-16"
+              className="max-h-24"
             />
           </a>
 
@@ -30,12 +30,12 @@ const Footer: React.FC = () => {
               </a>
             </div>
             <img
-              src="/images/img/datos_lab.png"
+              src="/images/img/datoslab_url_logo.png"
               alt="Logo DatosLab"
-              className="w-16 h-16"
+              className="w-32 "
             />
             <div className="flex gap-6 mt-2">
-              <a href="https://twitter.com/DatosLab_UPLA" target="_blank" rel="noopener noreferrer">
+              <a href="https://x.com/DatosLab_UPLA" target="_blank" rel="noopener noreferrer">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-6 w-6 fill-current">
                   <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
                 </svg>
@@ -59,7 +59,7 @@ const Footer: React.FC = () => {
               <img
                 src="images/img/logoacreditacion.png"
                 alt="Imagen acreditacion"
-                className="max-h-24"
+                className="max-h-32"
               />
             </a>
           </aside>
